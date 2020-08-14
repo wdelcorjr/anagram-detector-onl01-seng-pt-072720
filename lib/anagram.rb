31901d388e6092f943947c6f826e61e485c538ff
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-08-13 22:57:47 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(possibles)
+        possibles.select do |possible|
+            possible.split('').sort == @word.split('').sort 
+        end
+    end 
+end
